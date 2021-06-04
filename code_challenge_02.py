1c8b2d7f3fcff273245ec3138d419de89f5365d3
Scott Ford <swford@ualberta.ca>
2021-06-03 23:20:50 -0600

    completed coding challenge #2

---
+++ code_challenge_02.py
@@ -0,0 +1,23 @@
+# CodingNomads - Coding Challenge #2: Find the Floor
+# Scott Ford
+# June 3, 2021
+
+# You are on the ground floor (floor 0) of a tall apartment building. 
+# You need to get to a specific floor, but the only directions you have consist of a series of ( and ) characters:
+
+#     Every ( character means to go up one floor.
+#     Every ) character means to go down one floor.
+
+
+def find_floor(directions: str):
+    '''iterate through parentheses string to determine current floor'''
+    current_floor = 0
+    for direction in directions:
+        if direction == '(':
+            current_floor += 1
+        elif direction == ')':
+            current_floor -= 1
+    return current_floor
+
+current_floor = find_floor(')))')
+print(current_floor)
